673dd6a1fe3769ff9d714687e0f24afac0d6a26f
ShinG24 <ShinGgm24@gmail.com>
2023-09-05 14:07:52 +0900

    帰る前に

---
+++ Source/Graphics/PostProcessManager.cpp
@@ -0,0 +1,16 @@
+#include "PostProcessManager.h"
+
+
+namespace argent::graphics
+{
+	PostProcessManager::PostProcessManager(const GraphicsContext& graphics_context)
+	{
+
+	}
+
+	void PostProcessManager::Execute(const RenderContext& render_context)
+	{
+		
+	}
+
+}
